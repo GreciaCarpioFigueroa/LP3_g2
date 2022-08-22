5fe0f0536aa064f7cfcdb213289d2879632ee591
GreciaCarpioFigueroa <grecia.carpio@ucsm.edu.pe>
2022-08-22 10:37:54 -0500

    Actualización con multiplicaciones con el número ingresado

--- Principal.java
+++ Principal.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
-
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		int vida, rp =0,a=1;
+		boolean fl = false;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Número? ");
+		vida = sc.nextInt();
+		System.out.println("El número ingresado es: " + vida);
+		
+		for (int i = 0; i <= vida; i++)
+			rp += i;
+		System.out.println("La suma es: " + rp);
+		
+		for (int i = 1; i <= 12; i++)
+			System.out.println(vida +" multplicado por: " + i + " es igual a: " + (vida*i) );
+		//ola alslkaksl
 	}
 
 }
+
