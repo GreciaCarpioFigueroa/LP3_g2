7656da0d43d33cfe5a4de7f24ce9f795aeb83daa
GreciaCarpioFigueroa <grecia.carpio@ucsm.edu.pe>
2022-08-22 09:39:50 -0500

    Principal class muestra número ingresado

---
+++ Principal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Principal {
+
+	public static void main(String[] args) {
+		int vida,rp;
+		boolean fl = false;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Número? ");
+		vida = sc.nextInt();
+		System.out.println("El número ingresado es: " + vida);
+
+	}
+
+}
