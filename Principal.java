1f9920b40826d3047779f7aeea018b13f0a6690d
Grecia Carpio Figueroa <grecia.carpio@ucsm.edu.pe>
2022-08-27 18:28:16 -0500

    Correcion de interaccion entre clases

--- Principal.java
+++ Principal.java
@@ -5,9 +5,9 @@ public class Principal {
 		int vida, rp =0,a=1;
 		boolean fl = false;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Número? ");
+		System.out.println("Numero? ");
 		vida = sc.nextInt();
-		System.out.println("El número ingresado es: " + vida);
+		System.out.println("El numero ingresado es: " + vida);
 		
 		for (int i = 0; i <= vida; i++)
 			rp += i;
@@ -15,8 +15,31 @@ public class Principal {
 		
 		for (int i = 1; i <= 12; i++)
 			System.out.println(vida +" multplicado por: " + i + " es igual a: " + (vida*i) );
-		//ola alslkaksl
+		int a;
+		System.out.println("Encuentra la clave secreta");
+		a = sc.nextInt();
+		if (a != 9) {
+			System.out.println("No es el numero, perdiste :(");
+		}
+		else {
+			System.out.println("Encontraste tu numero de la suerte");
+			System.out.println("Te ganaste un premio");		
+			Regalo.elegir(vida)
+		}
 	}
-
 }
-
+public class Regalo {
+	public static String elegir(int vida) {
+		String gift = "";
+		switch (vida) {
+		case 1: gift = "Un pasaje al caribe"; break;
+		case 2: gift = "Una visita al museo mÃ¡s cercano a tu casa"; break;
+		case 3: gift = "Una entrada al cine"; break;
+		case 4: gift = "Una compra en un mall"; break;
+		case 5: gift = "Un chupetÃ­n"; break;
+		case 6: gift = "Ganaste 0.50"; break;
+		case 7: gift =  "Ganaste 0.10"; break;
+		}
+		return gift; 
+	}
+}
